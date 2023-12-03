7603d35ca38e9dee48cd6475326f8779031dd14a
sara1422002 <saraalii.142@icloud.com>
2023-12-03 13:55:21 +0200

    Commit1

---
+++ Algoriza_BE_333/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+
+namespace Algoriza_BE_333
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            
+
+        }
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env )
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
